4fd3086eeff08cdcc18093f787e4bda0322ef87f
kedhar-Git <kedharthodupunoori@gmail.com>
2026-07-21 16:23:43 +0530

    add

--- Practise/M01_Sliding_Window_&_Two_Pointers/S01/Two_Pointer_Optimization2.py
+++ Practise/M01_Sliding_Window_&_Two_Pointers/S01/Two_Pointer_Optimization2.py
@@ -1,5 +1,6 @@
 #26.Remove duplicates From sorted array
 from typing import List
+from xml.dom.minidom import Element
 def removeDuplicates(nums: List[int]) -> int:
     i=0
     for j in range(1,len(nums)):
@@ -9,6 +10,18 @@ def removeDuplicates(nums: List[int]) -> int:
     return i+1
 nums=[0,0,1,1,1,2,2,3,3,4]
 print(removeDuplicates(nums))
+
+#27.Remove Element
+def removeElement(nums: List[int], val: int) -> int:
+    i=0
+    for j in range(len(nums)):
+        if nums[j]!=val:
+            nums[i]=nums[j]
+            i+=1
+    return i
+nums=[3,2,2,3]
+val=3
+print(removeElement(nums, val))
 
 #167Two Sum II - Input array is sorted
 class Solution:
@@ -23,3 +36,6 @@ class Solution:
                 left += 1
             else:
                 right -= 1
+numbers = [2, 7, 11, 15] 
+target = 9
+solution = Solution()
