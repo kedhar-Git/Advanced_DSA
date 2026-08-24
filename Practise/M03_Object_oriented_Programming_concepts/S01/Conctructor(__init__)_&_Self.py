ce2f1df3ae446f9b728bcee9c8b949a9df7244e1
kedhar-Git <kedharthodupunoori@gmail.com>
2026-08-24 14:47:20 +0530

    changes

--- Practise/M03_Object_oriented_Programming_concepts/S01/Conctructor(__init__)_&_Self.py
+++ Practise/M03_Object_oriented_Programming_concepts/S01/Conctructor(__init__)_&_Self.py
@@ -36,4 +36,29 @@ class ParkingSystem:
             return True
         else:
             return False
+parkingSystem = ParkingSystem(1, 1, 0)
+print(parkingSystem.addCar(1))  
+print(parkingSystem.addCar(2))  
+print(parkingSystem.addCar(3))  
+print(parkingSystem.addCar(1))
 
+#1603. Design Parking System
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.slots = {
+            1: big,
+            2: medium,
+            3: small
+        }
+
+    def addCar(self, carType: int) -> bool:
+        if self.slots[carType] > 0:
+            self.slots[carType] -= 1
+            return True
+        return False
+parkingSystem = ParkingSystem(1, 1, 0)
+print(parkingSystem.addCar(1))
+print(parkingSystem.addCar(2))
+print(parkingSystem.addCar(3))
+print(parkingSystem.addCar(1))
